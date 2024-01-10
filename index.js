19186f88d3aaf1608393d9118e6e9fc518df0513
Fiamma <stavtxxx1@gmail.com>
2024-01-10 20:00:42 +0200

    add DB

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ const fs = require('fs')
 
 const db = JSON.parse(fs.readFileSync('./db.json', { encoding: 'utf-8' }));
 
+const finish = (finalDB) => {
+    const theNewOne = JSON.stringify(finalDB, null, 2);
+    fs.writeFileSync('./db.json', theNewOne);
+}
+
 const transfer = (theDatabase) => {
     if (theDatabase["not"].length === 0) {
         theDatabase["not"] = [...theDatabase["used"]];
         theDatabase["used"] = [];
-        const theNewOne = JSON.stringify(theDatabase, null, 2);
-        fs.writeFileSync('./db.json', theNewOne);
+        finish(theDatabase);
         console.log("All of the logical fallacies and biases were seen! Restarting the cycle.");
-        return;
+        console.log("Here is the new item of the day:");
     }
     randomNum = Math.floor(Math.random() * theDatabase["not"].length)
     itemOfTheDay = theDatabase["not"][randomNum]
     console.log(itemOfTheDay);
     theDatabase["used"].push(itemOfTheDay)
     theDatabase["not"].splice(randomNum, 1)
-    const theNewOne = JSON.stringify(theDatabase, null, 2)
-    fs.writeFileSync('./db.json', theNewOne)
+    finish(theDatabase);
     return;
 }
 
